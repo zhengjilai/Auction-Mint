a7498774019dbd8eb22561940f30c774c607b4ad
zhengjilai <zhengjilai5816@163.com>
2019-08-04 19:11:03 +0800

    add trace and update options

--- src/simulation/main.py
+++ src/simulation/main.py
@@ -6,7 +6,7 @@ if __name__ == '__main__':
 
     # initial parameters
     # initial total balance of the system
-    totalBalance = 1800000000000
+    totalBalance = 2400000000000
 
     # the block reward for a single round
     blockReward = 0.00004 * totalBalance
@@ -20,17 +20,16 @@ if __name__ == '__main__':
     # the number of bid winners
     bidWinner = 10
 
+    # if simulation needs to be traced
+    traceTag = True
+
     # the simulation object
-    simulation = Simulation(totalBalance, blockReward, miningExpense, bidWinner, transactionFees)
+    simulation = Simulation(totalBalance, blockReward, miningExpense, bidWinner, transactionFees, traceTag)
 
     # simulate round by round
     totalRound = 350000
     for i in range(totalRound):
         simulation.single_round()
-
-        # show simulation round
-        if i % 1000 == 0:
-            print(i)
 
     simulation.draw_time_series()
 
--- src/simulation/simulation.py
+++ src/simulation/simulation.py
@@ -6,28 +6,34 @@ from .draw import *
 # class for aucMint simulation
 class Simulation:
 
-    def __init__(self, tb, br, me, bw, tf):
+    def __init__(self, tb, br, me, bw, tf, tt):
 
         # system total balance
         self.totalBalance = tb
 
         # block reward
         self.blockReward = br
+        self.blockRewardRate = br / tb
 
         # average mining expense for a single miner
         self.miningExpense = me
+        self.miningExpenseRate = me / tb
 
         # the number of bid winners
         self.bidWinner = bw
 
         # the transaction fee predicted from total balance,
         self.transactionFeePredict = tf
+        self.fisherCoefficient = (tf / tb)
         # the number of former rounds used to predict the next round transaction fee
-        self.predictRoundNumber = 20
+        self.predictRoundNumber = 100
 
         # simulation round
         # initialized as 1
         self.round = 1
+
+        # if simulation needs to be traced
+        self.traceTag = tt
 
         # simulation result
         # [round, totalBalance, bidPrice. txFee]
@@ -43,15 +49,25 @@ class Simulation:
         # information for this round
         roundInfo = np.array([[self.round, self.totalBalance, bidPrice, self.transactionFeePredict]])
 
-        # concatenate self.result with info of this round
-        self.result = np.concatenate((self.result, roundInfo), axis = 0)
+        # data tracing
+        if self.traceTag:
+            self.trace_result(bidPrice)
 
-        # update totalBalance and transaction fee for the next round
+        # concatenate self.result with info of this round
+        self.result = np.concatenate((self.result, roundInfo), axis=0)
+
+        # update totalBalance, blockReward and miningCost for the next round
         self.__update_total_balance(bidPrice)
-        self.__update_predict_transaction_fee_from_total_balance()
+        # self.__update_block_reward_from_total_balance()
+        # self.__update_mining_cost_from_total_balance()
+        
+        # update transaction fee for the next round
+        res1 = self.__update_predict_transaction_fee_from_total_balance()
+        # res2 = self.__update_predict_transaction_fee_with_former_fee()
+        self.transactionFeePredict = res1
 
         # make perturbations
-        perturbation_type = 0
+        perturbation_type = 1
         self.__make_perturbations(perturbation_type)
 
         # update round number
@@ -90,19 +106,29 @@ class Simulation:
                           - self.result[self.round - self.predictRoundNumber][3]
                           + self.result[self.round][3]) / self.predictRoundNumber
 
+
+
         # modify the prediction with Fisher equation of exchange
-        self.transactionFeePredict = feeMidCalc / self.result[self.round][1] * self.totalBalance
+        return feeMidCalc / self.result[self.round][1] * self.totalBalance
 
     def __update_predict_transaction_fee_from_total_balance(self):
 
         # calc the predicted tx fee for the next round
+        # calculate the expected tx fee from total balance, with Fisher equation of exchange
+        expectedFee = self.totalBalance * self.fisherCoefficient
 
-        # the multiple between total balance and fee, with Fisher Equation
-        rate = self.result[0][1] / self.result[0][3]
-        # calculate the expected tx fee from total balance
-        expectedFee = self.totalBalance / rate
         # sample from nor distribution
-        self.transactionFeePredict = sample_from_norm_distribution(expectedFee, expectedFee / 40)
+        return sample_from_norm_distribution(expectedFee, expectedFee / 40)
+    
+    def __update_block_reward_from_total_balance(self):
+
+        # calc the block reward for the next round
+        self.blockReward = self.blockRewardRate * self.totalBalance
+
+    def __update_mining_cost_from_total_balance(self):
+
+        # calc the mining cost for the next round
+        self.miningExpense = self.miningExpenseRate * self.totalBalance
 
     def __make_perturbations(self, perturbation_type):
 
@@ -111,14 +137,26 @@ class Simulation:
         if perturbation_type == 0:
             if self.round == 100000:
                 self.totalBalance *= 0.975
-            if self.round == 200000:
+            elif self.round == 200000:
                 self.totalBalance *= 1.025
+        elif perturbation_type == 1:
+            if 150000 < self.round < 200000:
+                self.transactionFeePredict *= (1 + 0.1 * (self.round - 150000) / 50000)
+            elif self.round >= 200000:
+                self.transactionFeePredict *= 1.1
         else:
             pass
 
-    def print_result(self):
-
-        print(self.result)
+    def trace_result(self, bidPrice):
+        if self.round % 1000 == 0:
+            print("====================================")
+            print("Simulation Round: ", self.round)
+            print("Tx Fee: ", self.transactionFeePredict, self.transactionFeePredict / self.totalBalance)
+            print("Total balance: ", self.totalBalance, self.totalBalance / self.totalBalance)
+            print("Mining cost: ", self.miningExpense, self.miningExpense / self.totalBalance)
+            print("Block Reward: ", self.blockReward, self.blockReward / self.totalBalance)
+            print("Bid Price: ", bidPrice, bidPrice / self.totalBalance)
+            print("====================================")
 
     def draw_time_series(self):
 
